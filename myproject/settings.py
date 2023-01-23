0bf9b85ac942987b7e587c5ddb5498263da22575
Akash <av497131@gmail.com>
2023-01-24 01:59:32 +0530

    twerked

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
-from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middlewareWhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'myproject.urls'
@@ -125,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -147,4 +145,3 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'av497131@gmail.com'
 EMAIL_HOST_PASSWORD = 'Akashhai@1'
 
-django_heroku.settings(locals())
